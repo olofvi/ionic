65a7fcd2a7a1fb7252e795df517422988225f00a
olofvi <olofvi89@gmail.com>
2017-10-25 11:04:30 +0200

    add calculator tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CalculatorPage } from '../pages/calculator/calculator';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    TabsPage
+    TabsPage,
+    CalculatorPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    TabsPage
+    TabsPage,
+    CalculatorPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CalculatorPage } from '../calculator/calculator'
 import { AboutPage } from '../about/about';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-
+  calculatorTab = CalculatorPage;
   tab2Root = AboutPage;
 
   constructor() {
